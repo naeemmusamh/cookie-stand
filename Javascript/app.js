6436544044560ad21586c4cf879a415d093760a5
naeemmusamh <naeem@DESKTOP-RJ6QHBJ.localdomain>
2021-02-16 15:04:15 +0200

    add row for the total and some edit

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,8 +1,13 @@
 'use strict';
+let newarray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function randomValue(min, max) {
     return Math.ceil(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
+
+let divElement = document.getElementById('storeinfo');
+let table = document.createElement('table');
+divElement.appendChild(table);
 
 function SlamonCookies(name, min, max, averg) {
     this.name = name;
@@ -12,79 +17,93 @@ function SlamonCookies(name, min, max, averg) {
     this.workbyHour = [];
     this.cusmerbyHour1 = [];
     this.totalbyHour1 = [];
+    this.total1 = new Array();
     this.total = 0;
+}
 
-    SlamonCookies.prototype.headerhour = function() {
-        this.workbyHour = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
-    }
-    SlamonCookies.prototype.cusmerbyHour = function() {
-        for (let i = 0; i < this.workbyHour.length; i++) {
-            this.cusmerbyHour1.push(randomValue(this.max, this.min) * Math.ceil(this.min));
-            this.total = this.total + this.cusmerbyHour1[i];
-        }
-    }
 
-    SlamonCookies.prototype.totalbyHour = function() {
-        for (let i = 0; i < this.workbyHour.length; i++) {
-            this.totalbyHour1.push(Math.floor(this.averg * this.cusmerbyHour1[i]));
-            this.total = this.total + this.averg[i];
-        }
+SlamonCookies.prototype.headerhour = function() {
+    this.workbyHour = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
+}
+
+SlamonCookies.prototype.cusmerbyHour = function() {
+    for (let i = 0; i < this.workbyHour.length; i++) {
+        this.total1.push(randomValue(this.min, this.max) * Math.ceil(this.averg));
+        this.total = this.total + this.total1[i];
     }
-    SlamonCookies.prototype.body = function() {
-        let divElement = document.getElementById('storeinfo');
-        let articleElement = document.createElement('article');
-        divElement.appendChild(articleElement);
-        let table = document.createElement('table');
-        articleElement.appendChild(table);
-        let trElement = document.createElement('tr');
-        table.appendChild(trElement);
+    this.total1[this.total1.length] = this.total;
+}
+
+SlamonCookies.prototype.totalbyHour = function() {
+    for (let i = 0; i < this.workbyHour.length; i++) {
+        newarray[i] = newarray[i] + this.total1[i];
+        // console.log('total ( ' + i + ' ): ' + newarray[i] + ' cookie: ' + this.total[i]);
+    }
+    newarray[newarray.lenght] = this.total + this.total1;
+    // console.log(this.total1);
+}
+
+// SlamonCookies.prototype.cusmerbyHour = function() {
+//     for (let i = 0; i < this.workbyHour.length; i++) {
+//         this.cusmerbyHour1.push(randomValue(this.max, this.min) * Math.ceil(this.min));
+//         this.cusmerbyHour1[i] = this.cusmerbyHour1[i] + this.total;
+
+//     }
+// }
+
+// SlamonCookies.prototype.totalbyHour = function() {
+//     for (let i = 0; i < this.workbyHour.length; i++) {
+//         this.totalbyHour1.push(Math.floor(this.averg * this.cusmerbyHour1[i]));
+//         this.total = this.total + this.totalbyHour1[i];
+
+//     }
+
+// }
+
+SlamonCookies.prototype.body = function() {
+    let trElement = document.createElement('tr');
+    table.appendChild(trElement);
+    let thElement = document.createElement('th');
+    trElement.appendChild(thElement);
+    thElement.textContent = 'location';
+    for (let i = 0; i < this.workbyHour.length; i++) {
         let thElement = document.createElement('th');
         trElement.appendChild(thElement);
-        thElement.textContent = 'location';
-        for (let i = 0; i < this.workbyHour.length; i++) {
-            let thElement = document.createElement('th');
-            trElement.appendChild(thElement);
-            thElement.textContent = this.workbyHour[i];
-        }
-        let thElement1 = document.createElement('th');
-        trElement.appendChild(thElement1);
-        thElement1.textContent = 'Daily Location Total';
+        thElement.textContent = this.workbyHour[i];
     }
-    SlamonCookies.prototype.row = function() {
-        let divElement = document.getElementById('storeinfo');
-        let articleElement = document.createElement('article');
-        divElement.appendChild(articleElement);
-        let table = document.createElement('table');
-        articleElement.appendChild(table);
-        let trElement = document.createElement('tr');
-        table.appendChild(trElement);
+    let thElement1 = document.createElement('th');
+    trElement.appendChild(thElement1);
+    thElement1.textContent = 'Daily Location Total';
+}
+
+SlamonCookies.prototype.row = function() {
+    let trElement = document.createElement('tr');
+    table.appendChild(trElement);
+    let tdElement = document.createElement('th');
+    trElement.appendChild(tdElement);
+    tdElement.textContent = this.name;
+    for (let i = 0; i < this.total1.length; i++) {
         let tdElement = document.createElement('th');
         trElement.appendChild(tdElement);
-        tdElement.textContent = this.name;
-        for (let i = 0; i < this.workbyHour.length; i++) {
-            let tdElement = document.createElement('th');
-            trElement.appendChild(tdElement);
-            tdElement.textContent = this.cusmerbyHour1[i];
-        }
+        tdElement.textContent = this.total1[i];
     }
 }
+
 SlamonCookies.prototype.footer = function() {
-    let divElement = document.getElementById('storeinfo');
-    let articleElement = document.createElement('article');
-    divElement.appendChild(articleElement);
-    let table = document.createElement('table');
-    articleElement.appendChild(table);
     let trElement = document.createElement('tr');
     table.appendChild(trElement);
     let tdElement = document.createElement('th');
     trElement.appendChild(tdElement);
     tdElement.textContent = 'total';
-    for (let i = 0; i < this.workbyHour.length; i++) {
+    for (let i = 0; i < newarray.length; i++) {
         let tdElement = document.createElement('th');
         trElement.appendChild(tdElement);
-        tdElement.textContent = this.totalbyHour1[i];
+        tdElement.textContent = newarray[i];
     }
+    trElement.appendChild(tdElement);
+    tdElement.textContent = this.averg;
 }
+
 let Seattle = new SlamonCookies('Seattle', 23, 65, 6.3);
 Seattle.headerhour();
 Seattle.cusmerbyHour();
